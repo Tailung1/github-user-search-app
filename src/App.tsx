0e5352936129e54a0fbf9b10347d1ab97cd82a2a
Tailung1 <ramazsamitashvili11@gmail.com>
2025-02-22 06:58:23 +0400

    add clean function

--- src/App.tsx
+++ src/App.tsx
@@ -14,18 +14,25 @@ function App() {
   const [search,setSearch]=useState<string>("")
 
   const fetchFunc = async () => {
+    if(!search.trim()) return;
     try {
         const response= await fetch(`https://api.github.com/users/${search}`)
         const jsonData= await response.json()
         setUser(jsonData)
     } catch(error){}
   };
+  const handleClear=()=> {
+    setSearch("");
+    setUser(null)
+  }
   return (
     <>
-      <input onChange={(event) => setSearch(event.target.value)} type="text" />
+      <input onChange={(event) => setSearch(event.target.value)} value={search} type="text" />
       <button onClick={fetchFunc}>Search user</button>
+      <button onClick={handleClear} style={{marginLeft:"10px"}}>Clear Input</button>
       <h1>{user?.name ?? "User Name will appear here"}</h1>
       <h2>{user?.login ?? "User Login will apprear here"}</h2>
+
     </>
   );
 }
