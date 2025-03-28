53f695ac34b91336c732566a35e377d58198e518
Tailung1 <ramazsamitashvili11@gmail.com>
2025-03-28 21:13:21 +0400

    repeat

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,20 @@
 import { useEffect, useState } from "react";
+import axios from "axios";
 
-import "./App.css";
+export default function App() {
+  const [user, setUser] = useState<string>("");
 
-type TUser = {
-  name: string | null;
-  login: string | null;
-};
-
-function App() {
-  const [user, setUser] = useState<TUser | null>(null);
-
-  const [search, setSearch] = useState<string>("");
-  useEffect(() => setUser({ name: "Tailung1", login: "tailung1" }), []);
-
-  const fetchFunc = async () => {
-    if (!search.trim()) return;
-    try {
-      const response = await fetch(`https://api.github.com/users/${search}`);
-      const jsonData = await response.json();
-      setUser(jsonData);
-    } catch (error) {}
-  };
-  const handleClear = () => {
-    setSearch("");
-    setUser(null);
-  };
-  return (
-    <>
-      <input
-        onChange={(event) => setSearch(event.target.value)}
-        value={search}
-        type="text"
-      />
-      <button onClick={fetchFunc}>Search user</button>
-      <button onClick={handleClear} style={{ marginLeft: "10px" }}>
-        Clear Input
-      </button>
-      <h1>{user?.name ?? "User Name will appear here"}</h1>
-      <h2>{user?.login ?? "User Login will apprear here"}</h2>
-    </>
-  );
+  useEffect(() => {
+    const fetchUser = async () => {
+      const response = await axios.get(
+        "https://api.github.com/users/Tailung1"
+      );
+      if (response.status === 200) {
+        const data = await response.data;
+        setUser(data);
+      }
+    };
+    fetchUser();
+  }, []);
+  return <div>{user?.created_at || " cant found"}</div>;
 }
-
-export default App;
